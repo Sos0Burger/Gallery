228d87e2cf6ad8b57ad8b4b440075eb9759e7f03
SosoBurger <stulnikovan1337@gmail.com>
2022-05-12 23:11:01 +0300

    Images switch

--- app/src/main/java/ru/example/mygallery/ImgviewActivity.java
+++ app/src/main/java/ru/example/mygallery/ImgviewActivity.java
@@ -6,7 +6,10 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.github.chrisbanes.photoview.PhotoView;
 
@@ -15,7 +18,6 @@ import java.util.ArrayList;
 public class ImgviewActivity extends AppCompatActivity {
     int position;
     String[] pathList;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,8 +25,27 @@ public class ImgviewActivity extends AppCompatActivity {
         Intent intent = getIntent();
         pathList = intent.getStringArrayExtra("pathList");
         position = intent.getIntExtra("Position", 0);
-
         PhotoView photoView = (PhotoView) findViewById(R.id.photo_view);
         photoView.setImageBitmap(BitmapFactory.decodeFile(pathList[position]));
+    }
+    public void nextImg(View v){
+        if (position<pathList.length-1){
+            PhotoView photoView = (PhotoView) findViewById(R.id.photo_view);
+            position++;
+            photoView.setImageBitmap(BitmapFactory.decodeFile(pathList[position]));
+        }
+        else{
+            Toast.makeText(getApplicationContext(), "Фото больше нет", Toast.LENGTH_SHORT).show();
+        }
+    }
+    public void previousImg(View v){
+        if (position!=0){
+            PhotoView photoView = (PhotoView) findViewById(R.id.photo_view);
+            position--;
+            photoView.setImageBitmap(BitmapFactory.decodeFile(pathList[position]));
+        }
+        else{
+            Toast.makeText(getApplicationContext(), "Фото больше нет", Toast.LENGTH_SHORT).show();
+        }
     }
 }
